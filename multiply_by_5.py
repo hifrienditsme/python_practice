3ecda4aa4c6e53fa41c11216b0d40e1adf86801a
hifrienditsme <lara.m.merrick@gmail.com>
2024-12-09 19:29:35 -0600

    dec9commit

--- multiply_by_5.py
+++ multiply_by_5.py
@@ -2,6 +2,7 @@
 #The program should function like the following sample output:
 #Please type in a number: 3
 #3 times 5 is 15
+
 user_number = int(input("Please type in a number: "))
 times_five = user_number * 5
 

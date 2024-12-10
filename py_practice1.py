35615e63e38fbea29988963aae89f6b8f76d5273
hifrienditsme <lara.m.merrick@gmail.com>
2024-12-09 18:18:59 -0600

    first commit

---
+++ py_practice1.py
@@ -0,0 +1,8 @@
+#Please write a program which asks the user for a number. The program then prints out the number as multiplied by five.
+#The program should function like the following sample output:
+#Please type in a number: 3
+#3 times 5 is 15
+user_number = int(input("Please type in a number: "))
+times_five = user_number * 5
+
+print(f"{user_number} times 5 is {times_five}")
